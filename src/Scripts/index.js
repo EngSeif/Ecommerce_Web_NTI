10d4266d410b2757bc7e9c34c614ab16055a2bd0
EngSeif <eng.seifmohamed2004@gmail.com>
2024-09-06 15:15:29 +0300

    Add Files V2

---
+++ src/Scripts/index.js
@@ -0,0 +1,14 @@
+'use strict'
+const menuItems = document.querySelector('.Menu-items')
+document.getElementById("Menu-button").addEventListener('click', (e) => {
+    e.target.name = e.target.name === 'menu' ? 'close' : 'menu'
+    menuItems.classList.toggle('top-[6.12%]')
+})
+
+
+const heartIcons= document.getElementsByClassName('heart');
+for (const heartIcon of heartIcons) {
+    heartIcon.addEventListener('click', () => {
+        heartIcon.style.color = heartIcon.style.color === "" ? "red" : "";
+    })
+}
